3555c73df649363e0209108d27d3134c90186b7f
abjeet16 <avjeetyadav00@gmail.com>
2024-04-25 21:06:26 +0530

    doing

---
+++ app/src/main/java/com/example/chatbot/View/fragment/SplashFragment.kt
@@ -0,0 +1,51 @@
+package com.example.chatbot.View.fragment
+
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.Window
+import android.view.WindowManager
+import androidx.core.content.ContextCompat
+import androidx.core.view.WindowCompat
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.shreebhagavatgita.R
+
+
+class SplashFragment : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        changeStatusBarColor()
+        Handler(Looper.getMainLooper()).postDelayed({
+            findNavController().navigate(R.id.action_splashFragment_to_homeFragment)
+        },3000)
+        return inflater.inflate(R.layout.fragment_splash, container, false)
+    }
+
+    private fun changeStatusBarColor() {
+        val window = activity?.window
+        if (window != null) {
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+        }
+        if (window != null) {
+            window.statusBarColor = ContextCompat.getColor(requireContext(), R.color.statusBarColor)
+        }
+        if (window != null) {
+            WindowCompat.getInsetsController(window, window.decorView).apply {
+                isAppearanceLightStatusBars = true
+            }
+        }
+    }
+}
